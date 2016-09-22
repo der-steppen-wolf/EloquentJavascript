9c9fc184276760477e0bf1c3bab8213936981e47
meta-morphosis <theodorapanou2@gmail.com>
2016-09-22 17:41:39 +0300

    Update ch02.js

--- ch02.js
+++ ch02.js
@@ -1,9 +1,9 @@
 /* Looping a triangle */
 
 function printTriangle() {
-  for (var i = 0; i <= 7; i++) {
-  	for (var j = 0, row = ''; j < i; j++)
-      row += '#';
+    for (var i = 0; i <= 7; i++) {
+    	for (var j = 0, row = ''; j < i; j++)
+    	    row += '#';
   	console.log(row);
   }
 }
@@ -26,11 +26,11 @@ printTriangle();
 /* Chess board */
 
 function chessBoard() {
-  	var board = '';
-	for (var row = 0; row < 8; row++) {
-      	for (var col = 0; col < 8; col++) {
-          	if (row % 2 == 0)	board += (col % 2 == 0) ? ' ': '#';
-          	else	board += (col % 2 == 0) ? '#': ' ';
+    var board = '';
+    for (var row = 0; row < 8; row++) {
+	for (var col = 0; col < 8; col++) {
+	    if (row % 2 == 0)	board += (col % 2 == 0) ? ' ': '#';
+            else	board += (col % 2 == 0) ? '#': ' ';
       	}
       	board += '\n';
     }
@@ -38,7 +38,7 @@ function chessBoard() {
 }
 
 console.log(chessBoard());
-// →  # # # #
+// →      # # # #
 //	 # # # # 
 //	  # # # #
 //	 # # # # 
